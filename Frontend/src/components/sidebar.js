b50ea69cb395b1b178b4082d4c02f75e4dd77d9c
subhashprasad2001@gmail.com <{96110162+subhashdippu@users.noreply.github.com}+{subhashdippu}@users.noreply.github.com>
2026-01-22 09:59:49 +0530

    feat: adding schedule-email

--- Frontend/src/components/sidebar.js
+++ Frontend/src/components/sidebar.js
@@ -1,7 +1,7 @@
 // Radha
 import React from "react";
 import { Link, Outlet } from "react-router-dom";
-// import ScheduleEmail from "./scheduledEmail";
+import ScheduleEmail from "./scheduledEmail";
 import { ComposeEmail } from "../App";
 export default function EmailDashboard() {
   return (
@@ -30,7 +30,7 @@ export default function EmailDashboard() {
 
         <div className="text-xs text-gray-400 mb-2">CORE</div>
         <nav className="space-y-2">
-          <Link to="schedule-email">
+          <Link to="schedle-email">
             <div className="flex items-center justify-between px-3 py-2 rounded-md bg-green-50 text-green-700 font-medium">
               <span>Scheduled</span>
               <span className="text-xs">12</span>
@@ -48,7 +48,7 @@ export default function EmailDashboard() {
       {/* RIGHT SIDE CONTENT */}
       <main className="flex-1">
         <Outlet />
-        {/* <ScheduleEmail /> */}
+        <ScheduleEmail />
         <ComposeEmail />
       </main>
     </div>
--- Frontend/src/routers/router.js
+++ Frontend/src/routers/router.js
@@ -1,5 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../pages/main";
+import ScheduledEmail from "../components/scheduledEmail";
 import ComposeEmail from "../components/composeEmail";
 
 const router = createBrowserRouter([
@@ -7,6 +8,10 @@ const router = createBrowserRouter([
     path: "/",
     element: <Main />,
     children: [
+      {
+        path: "/schedle-email",
+        element: <ScheduledEmail />,
+      },
       {
         path: "/compose-email",
         element: <ComposeEmail />,
